5820cbb29901215ffeb09923ebe181353cc3e4fe
Akiicat <aaaa1379@gmail.com>
2018-01-23 14:01:36 +0800

    remove comment

--- spec/m3u8_spec.cr
+++ spec/m3u8_spec.cr
@@ -1,5 +1,4 @@
 require "./spec_helper"
 
 describe M3U8 do
-  # TODO: Write tests
 end
--- src/m3u8.cr
+++ src/m3u8.cr
@@ -1,6 +1,6 @@
 require "big"
 require "./patch/*"
-require "./m3u8/concern" # preload
+require "./m3u8/concern"
 require "./m3u8/*"
 
 module M3U8
@@ -9,12 +9,4 @@ module M3U8
 
   include Protocol
 end
-
-# file = File.read("spec/playlists/playlist.m3u8")
-file = File.read("spec/playlists/timestamp_playlist.m3u8")
-M3U8::Parser.read file
-file = File.read("spec/playlists/iframes.m3u8")
-M3U8::Parser.read file
-# scan = M3U8::Scanner.new file
-# pp scan.current_line
 
